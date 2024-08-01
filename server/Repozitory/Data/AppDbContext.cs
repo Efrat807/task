e18e0aba8a69d3dccd61d56199e0bf0172f32b67
Efrat Shefer <efkeynan@gmail.com>
2024-08-01 13:12:19 +0300

    forgot to commit this changes

--- server/Repozitory/Data/AppDbContext.cs
+++ server/Repozitory/Data/AppDbContext.cs
@@ -13,7 +13,6 @@ namespace Repozitory.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
-        //public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingList> ShoppingLists { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
